859a5154fcdbe6e7228bced807e669d2c5e02cfa
Steven Davies-Cortes <43970645+StivDC@users.noreply.github.com>
2019-12-18 14:20:12 +0000

    cheeky day 2 solution using lists

---
+++ Steve_Code/day2.py
@@ -0,0 +1,25 @@
+import math
+
+f = open("intCode_day2.txt", "r")
+def getIntCode(f):
+	# The cheating way of doing this intCode shit in python 
+	for x in f: y = x
+	k = list(y.split(","))
+	return [int(x) for x in k]
+
+k = getIntCode(f)
+
+# Setting the "1202 Program alarm" state
+k[1] = 12
+k[2] = 2
+
+for i in range(0, len(k), 4):
+	if k[i] == 99:
+		break
+	else:
+		a, b, c = k[int(i+3)], k[int(i+1)], k[int(i+2)]
+		if k[i] == 1:
+			k[a] = k[b] + k[c]
+		elif k[i] == 2:
+			k[a] = k[b] * k[c]
+print(k)
