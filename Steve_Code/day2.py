04a646b361157bdef6e6d7318931949ca4dc29d8
Steven Davies-Cortes <43970645+StivDC@users.noreply.github.com>
2019-12-19 17:06:27 +0000

    Finished part 2, was easier to merge into one file than create a seperate one

--- Steve_Code/day2.py
+++ Steve_Code/day2.py
@@ -13,13 +13,30 @@ k = getIntCode(f)
 k[1] = 12
 k[2] = 2
 
-for i in range(0, len(k), 4):
-	if k[i] == 99:
-		break
-	else:
-		a, b, c = k[int(i+3)], k[int(i+1)], k[int(i+2)]
-		if k[i] == 1:
-			k[a] = k[b] + k[c]
-		elif k[i] == 2:
-			k[a] = k[b] * k[c]
-print(k[0])
+def iCodeEval(k):
+	k = k[:]
+	for i in range(0, len(k), 4):
+		if k[i] == 99:
+			break
+		else:
+			a, b, c = k[int(i+3)], k[int(i+1)], k[int(i+2)]
+			if k[i] == 1:
+				k[a] = k[b] + k[c]
+			elif k[i] == 2:
+				k[a] = k[b] * k[c]
+	return k
+
+print(iCodeEval(k)[0])
+
+# Part 2
+
+for n in range(100):
+	for v in range(100):
+		k[1], k[2] = n, v
+		try:
+
+			if iCodeEval(k)[0] == 19690720:
+				print(100 * n + v)
+				break
+		except IndexError as ex:
+			pass
